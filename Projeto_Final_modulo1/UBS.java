6f00dd40c8d8af53d4c85a5378fd0824af23e3eb
Dislani <dismelo16@gmail.com>
2022-07-18 22:15:45 -0300

    Codigo do projeto apresentado no encerramento do primeiro modulo da Generation

---
+++ Projeto_Final_modulo1/UBS.java
@@ -0,0 +1,14 @@
+package projetoFinalBloco1;
+
+	public interface UBS {
+		
+		// MÉTODOS DA INTERFACE
+				
+			void temperatura(double testeTemperatura);
+			void avisoTemperatura();
+			void cartaoVac(boolean CartaoVac);
+				
+	}
+
+
+
